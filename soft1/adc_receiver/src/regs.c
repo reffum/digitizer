e85106ad2fe3540d941e324bb97b7e7829c2675a
reffum <reffum@bk.ru>
2020-04-06 20:36:12 +0300

    Rename software to soft1. I need to do this, because in VITIS all path only absolut and it contain "soft1" anywhere. May it be soon.

--- soft1/adc_receiver/src/regs.c
+++ soft1/adc_receiver/src/regs.c
@@ -12,6 +12,7 @@
 #define ADDR_DSIZE		3
 
 #define _CONTROL_START	0x1
+#define _CONTROL_TEST	0x2
 
 static const uint16_t ID_VALUE = 0x55AA;
 
@@ -29,6 +30,10 @@ int reg_read(uint16_t addr, uint16_t* value)
 		break;
 	case ADDR_CONTROL:
 		*value = 0;
+
+		if(adc_input_get_test())
+			*value |= _CONTROL_TEST;
+
 		break;
 	case ADDR_DSIZE:
 		*value = adc_input_get_size() >> 16;
@@ -45,6 +50,11 @@ int reg_write(uint16_t addr, uint16_t* value)
 	switch(addr)
 	{
 	case ADDR_CONTROL:
+		if(*value & _CONTROL_TEST)
+			adc_input_set_test(true);
+		else
+			adc_input_set_test(false);
+
 		if(*value & _CONTROL_START)
 			data_channel_send();
 		break;

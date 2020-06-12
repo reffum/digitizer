700052ec597097302b37909eb14698261029062a
reffum <reffum@bk.ru>
2020-06-12 14:39:11 +0300

    Change code.

--- soft1/adc_receiver/src/periph/pwm.c
+++ soft1/adc_receiver/src/periph/pwm.c
@@ -23,8 +23,8 @@ void pwm_init(void)
 	XTtcPs_SetOptions(&TtcPsInst, XTTCPS_OPTION_INTERVAL_MODE | XTTCPS_OPTION_MATCH_MODE | XTTCPS_OPTION_WAVE_POLARITY | XTTCPS_OPTION_WAVE_DISABLE);
 
 	//TODO: For debug only
-	pwm_set_freq(1000);
-	pwm_set_dc(10);
+	pwm_set_freq(10);
+	pwm_set_dc(1);
 	pwm_enable();
 }
 
